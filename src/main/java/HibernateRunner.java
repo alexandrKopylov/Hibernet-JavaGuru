69a0079fbc52e073afc3062a8926aeccc9fdb2df
Sasha <sash-2007@ya.ru>
2023-11-17 15:46:48 +0500

    na rabote  add  template   builder  , not  lambok

--- src/main/java/HibernateRunner.java
+++ src/main/java/HibernateRunner.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 
 import java.time.LocalDate;
 
+
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
@@ -17,22 +18,19 @@ public class HibernateRunner {
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession();) {
-
             session.beginTransaction();
 
-            User user = User.builder()
-                    .username("iva123n@mail.ru")
-                    .firstname("ivan")
-                    .lastname("Ivaniv")
-                    .birthDate(
-                            new Birthday(LocalDate.of(2000, 01, 01)) )
-
-                    .role(Role.USER)
+          User user =   new User.UserBuilder()
+                    .setUserName("111")
+                    .setFirstName("alexx")
+                    .setLastName("Kop")
+                    .setBirthDate( new Birthday(LocalDate.of(2000, 01, 01)) )
+                    .setRole(Role.USER)
                     .build();
 
             // базовые команды  в нhibernete
 
-        //    session.save(user);      // сохраняем юзера в базу даннных
+       //     session.save(user);      // сохраняем юзера в базу даннных
          //  session.update(user);     // будет обновлять юзера ,  может выдать исключение
                                              // если такого юзера не найдется
 
@@ -41,7 +39,7 @@ public class HibernateRunner {
 
           ///  session.delete(user);      //  удаляет узера
 
-           User user1 =  session.get (User.class , "iva123n@mail.ru");   // получить юзера  из базы данных по ключу id
+           User user1 =  session.get (User.class , "111");   // получить юзера  из базы данных по ключу id
 
             System.out.println(user1);
 
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -8,23 +8,84 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+// @Builder    билдер напишу  сам
 @Entity
-@Table (name ="users")
+@Table(name = "users")
 public class User {
     @Id
     private String username;
     private String firstname;
     private String lastname;
-
-
-    @Convert (converter = BirthdayConverter.class)
-    @Column (name = "birth_date")
+    @Convert(converter = BirthdayConverter.class)
+    @Column(name = "birth_date")
     private Birthday birthDate;
-
-    @Enumerated (EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    private User() {
+    }
+
+    private User(UserBuilder userBuilder) {
+        this.username = userBuilder.username;
+        this.firstname = userBuilder.firstname;
+        this.lastname = userBuilder.lastname;
+        this.birthDate = userBuilder.birthDate;
+        this.role = userBuilder.role;
+    }
+
+    public static class UserBuilder {
+        private String username;
+        private String firstname;
+        private String lastname;
+        private Birthday birthDate;
+        private Role role;
+
+        public UserBuilder() {
+        }
+
+        public UserBuilder setUserName(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder setFirstName(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public UserBuilder setLastName(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public UserBuilder setBirthDate(Birthday birthDate) {
+            this.birthDate = birthDate;
+            return this;
+        }
+
+        public UserBuilder setRole(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birthDate=" + birthDate +
+                ", role=" + role +
+                '}';
+    }
+
+
 }
